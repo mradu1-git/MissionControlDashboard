824161d0cf0772ff79a33adf6c456c5831f884b4
Mradul Pal <mradulpal@outlook.com>
2026-06-16 09:16:54 +0530

    main.cpp updated

--- main.cpp
+++ main.cpp
@@ -1,18 +1,17 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "missioncontroller.h"
+#include "vehicle.h"
+#include "vehiclemodel.h"
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char* argv[]) {
     QGuiApplication app(argc, argv);
-
+    qmlRegisterType<VehicleModel>("MissionDashboard", 1, 0, "VehicleModel");
+    qmlRegisterUncreatableType<Vehicle>("MissionDashboard", 1, 0, "Vehicle", "Vehicle is created by Vehicle Model");
     QQmlApplicationEngine engine;
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.loadFromModule("MissionControlDashboard", "Main");
-
-    return QGuiApplication::exec();
+    MissionController controller;
+    engine.rootContext()->setContextProperty("controller", &controller);
+    engine.loadFromModule("MissionDashboard", "Main");
+    return app.exec();
 }
