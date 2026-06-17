e30bdd0505077d765969f216ea5b31891d7892d7
Mradul Pal <mradulpal@outlook.com>
2026-06-17 23:48:48 +0530

    Vehicle.cpp added

--- vehicle.cpp
+++ vehicle.cpp
@@ -1,3 +1,19 @@
 #include "vehicle.h"
 
-Vehicle::Vehicle() {}
+Vehicle::Vehicle(QObject* parent) : QObject(parent),
+    m_name("Unknown"),
+    m_status(Idle),
+    m_battery(100.0),
+    m_latitude(0.0),
+    m_longitude(0.0)
+{}
+
+Vehicle::Vehicle(const QString &name, Status status, double battery, double lat, double lon, QObject* parent) : QObject(parent)
+    , m_name(name)
+    , m_status(status)
+    , m_battery(battery)
+    , m_latitude(lat)
+    , m_longitude(lon)
+{}
+
+
--- vehiclemodel.h
+++ vehiclemodel.h
@@ -1,10 +1,34 @@
-#ifndef VEHICLEMODEL_H
-#define VEHICLEMODEL_H
+#pragma once
+#include <QAbstractTableModel>
+#include <QList>
+#include <QtQml/qqml.h>
+#include "vehicle.h"
 
-class VehicleModel
+class VehicleModel : public QAbstractListModel
 {
+    Q_OBJECT
+    QML_ELEMENT
 public:
-    VehicleModel();
-};
+    enum VehicleRoles {
+        NameRole = Qt::UserRole + 1,
+        StatusRole,
+        BatteryRole,
+        LatitudeRole,
+        LongitudeRole,
+        VehicleObjectRole
+    };
+    explicit VehicleModel(QObject *parent = nullptr);
 
-#endif // VEHICLEMODEL_H
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role) const override;
+    QHash<int, QByteArray> roleNames() const override;
+
+    Q_INVOKABLE void addVehicle(const QString &name);
+    Q_INVOKABLE void removeVehicle(int index);
+    Q_INVOKABLE void updateStatus(int index, Vehicle::Status status);
+    Q_INVOKABLE void updateBattery(int index, double battery);
+    Q_INVOKABLE Vehicle* getVehicle(int index);
+private:
+    QList<Vehicle*> m_vehicles;
+};
+// VEHICLEMODEL_H
